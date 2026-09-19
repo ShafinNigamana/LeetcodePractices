3fc165c7c763312f9d8826dbacf43df845666296
Shafin Nigamana <Shafin.nigamana@gmail.com>
2026-09-19 23:54:08 +0530

    [217] [Time Beats: 62.59%] [Memory Beats: 63.01%] - LeetPush

--- 217-contains-duplicate/217-Contains-Duplicate.cpp
+++ 217-contains-duplicate/217-Contains-Duplicate.cpp
@@ -1,13 +1,13 @@
 class Solution {
 public:
     bool containsDuplicate(vector<int>& nums) {
-        int n = nums.size();
-        sort(nums.begin(), nums.end());
+        unordered_set<int> myset;
 
-        for(int i = 1; i<n; i++){
-            if(nums[i] == nums[i-1]){
+        for(int i : nums){
+            if(myset.find(i) !=  myset.end()){
                 return true;
             }
+            myset.insert(i);
         }
         return false;
     }
